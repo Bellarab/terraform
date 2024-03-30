7ff9dd6524c526aec3ca9fea166a4eae2d9015b8
Nassim <bellarabnassim@gmail.com>
2024-03-30 23:05:12 +0000

    mm

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "ec2_example" {
 }
 
 resource "aws_key_pair" "deployer" {
-  key_name   = "keyy"           
+  key_name   = "keyy"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPrqeNmm3ZfnoaX9IpmW0PUPWhnpIad9XEetnL9mfpVuVX3dUJAa8kewgZHtN/aAjGUBoOHQ8tgEs+XuGOOxYRS5JObW56jgIJaz6DlCpwf0hqxpdX+jVeRtO+hF0mzx36T/n/HiMmndaLsRuhlVUuNYw/XQA3mPL92Rlq/EyR1ZHVSg6DdjEPIIYNjfC8L8+mv3BPJgt26vl+Pv3cFmWpzfoQlinlzg/xmAHqYxiWTKMWLWCaggEhb6MMShvIQjWiA5rsmT5GdpR0p0VEeAo3iqfnBHF4U20LTCtz24us3PSCn9qGUG3a7Ztf8fj4GoSjP/pbfhkywliXcadUOXnN nassim@DESKTOP-C2DPHLV"
-     
+
 }
