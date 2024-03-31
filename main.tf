5c47b35160c0a280d25efebd09878ef73f0c77dc
Nassim <bellarabnassim@gmail.com>
2024-03-31 13:35:44 +0000

    mmm

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAXYKJQW65T77VDAN2"
-  secret_key = "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"
+  access_key = "AKIAXYKJQW65ZY7KRZXD"
+  secret_key = "dThXHRkFKFapRXPzrkix4D4JeQpf7PIannUUzFTM"
 }
+
+
 
 resource "aws_instance" "ec2_example" {
   ami           = "ami-08116b9957a259459"
@@ -18,4 +20,12 @@ resource "aws_key_pair" "deployer" {
   key_name   = "keyy"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPrqeNmm3ZfnoaX9IpmW0PUPWhnpIad9XEetnL9mfpVuVX3dUJAa8kewgZHtN/aAjGUBoOHQ8tgEs+XuGOOxYRS5JObW56jgIJaz6DlCpwf0hqxpdX+jVeRtO+hF0mzx36T/n/HiMmndaLsRuhlVUuNYw/XQA3mPL92Rlq/EyR1ZHVSg6DdjEPIIYNjfC8L8+mv3BPJgt26vl+Pv3cFmWpzfoQlinlzg/xmAHqYxiWTKMWLWCaggEhb6MMShvIQjWiA5rsmT5GdpR0p0VEeAo3iqfnBHF4U20LTCtz24us3PSCn9qGUG3a7Ztf8fj4GoSjP/pbfhkywliXcadUOXnN nassim@DESKTOP-C2DPHLV"
 
+}
+
+terraform {
+  backend "s3" {
+    bucket = "mon-nouveau-bucket-unique"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+  }
 }
